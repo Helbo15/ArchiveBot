77352047c268e822aa7cb96d182efab8bbfc1742
Helbo15 <helbo15@gmail.com>
2018-04-26 20:40:21 +0200

    First archivebot Version
    first version of a simple Python bot for discord that can archive a message in a archive channel

---
+++ ArchiveBot.py
@@ -0,0 +1,32 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("!Arkiver"):
+        archiveChannel =  message.content.split(' ')[0].split("!Arkiver")[1]
+        print(archiveChannel)
+        for channel in client.get_all_channels():
+            if channel.name.startswith(archiveChannel):
+                await client.send_message(channel, message.author.mention + " wrote:   " + message.content)
+
+    if  client.user.mentioned_in(message):
+        archiveChannel =""
+        archiveChannel =  message.content.split(' ')[1]
+        print(archiveChannel)
+        for channel in client.get_all_channels():
+            if channel.name.startswith(archiveChannel):
+                await client.send_message(channel, message.author.mention + " wrote:   " + message.content)
+
+client.run('NDM5MDU5OTQyMDc1NTMxMjY1.DcNp_Q.1gwXYuKJj0KHITX-eebFDQ_h1Jw')
